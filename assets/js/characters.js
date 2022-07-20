7d430ae5f67e9f62bb621f6d585a94943cbdc7ca
Vinicius Boschi <vinicius.yg@gmail.com>
2022-07-20 15:51:06 -0300

    Adicionando página dos personagens

---
+++ assets/js/characters.js
@@ -0,0 +1,25 @@
+table()
+
+async function table() {
+    const response = await swapiGet("people/")
+    const tableData = response.data.results
+    tableData.forEach(function(people) {
+        $('#filmsTable').append(
+            `<tr>
+                <td>${people.name}</td>
+                <td>${people.height/100 + 'm'}</td>
+                <td>${people.mass}</td>
+                <td>${people.hair_color}</td>
+                <td>${people.skin_color}</td>
+                <td>${people.eye_color}</td>
+                <td>${people.birth_year}</td>
+                <td>${people.gender}</td>
+            </tr>`
+        )
+    })
+    console.log(tableData)
+}
+
+function swapiGet(param) {
+    return axios.get(`https://swapi.dev/api/${param}`)
+}
