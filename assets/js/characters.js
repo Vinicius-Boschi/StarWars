6c3814b9b3104eff74bfe22a77978c1e093d8a01
Vinicius Boschi <vinicius.yg@gmail.com>
2022-07-22 14:17:48 -0300

    Trocando o menu lateral por navbar e fazendo a página de characters

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -4,18 +4,30 @@ async function table() {
     const response = await swapiGet("people/")
     const tableData = response.data.results
     tableData.forEach(function(people) {
-        $('#filmsTable').append(
-            `<tr>
-                <td>${people.name}</td>
-                <td>${people.height/100 + 'm'}</td>
-                <td>${people.mass}</td>
-                <td>${people.hair_color}</td>
-                <td>${people.skin_color}</td>
-                <td>${people.eye_color}</td>
-                <td>${people.birth_year}</td>
-                <td>${people.gender}</td>
-            </tr>`
-        )
+        $('#charactersSection').append (
+            `
+        <div class="cardContainer">
+          <div class='cardCharacter'>
+            <h1>${people.name}</h1>
+            <div class='imageContainer'>
+              <img src='https://starwars-visualguide.com/assets/img/characters/${people.url.replace(/\D/g,"")}.jpg' alt='${people.name}'>
+            </div>
+            <div class='infoContainer'>
+              <h2>About:</h2>
+              <div class='infoContent'>
+                <h3>Name: ${people.name}</h3>
+                <h3>Height: ${people.height/100 + 'm'}</h3>
+                <h3>Mass: ${people.mass}</h3>
+                <h3>Hair: ${people.hair_color}</h3>
+                <h3>Skin: ${people.skin_color}</h3>
+                <h3>Eye: ${people.eye_color}</h3>
+                <h3>Birth: ${people.birth_year}</h3>
+                <h3>Gender: ${people.gender}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        `)
     })
 }
 
---
+++ assets/js/navbar.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".button")
+const container = document.querySelector(".links-container")
+const list = document.querySelector(".list")
+
+button.addEventListener("click", () => {
+    const height = container.getBoundingClientRect().height
+    const linksHeight = list.getBoundingClientRect().height
+
+    if (height === 0) {
+        container.style.height = `${linksHeight}px`
+    } else {
+        container.style.height = 0
+    }
+})
